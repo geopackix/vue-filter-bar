e51f86987e7aaa6fa309aae7059366826627c549
Manuel Hart <manuel.hart@geokoord.com>
2020-05-18 05:46:36 +0200

    preparations for multiselect 2

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ const port = 3001
 //make 'example' folder available.
 app.use('/', express.static(__dirname + '/example'));
 app.use('/src', express.static(__dirname + '/src'));
+app.use('/assets', express.static(__dirname + '/src/assets'));
 
 app.listen(port, function () {
   console.log('Example is running on port ' + port);
--- example/example-controller.js
+++ example/example-controller.js
@@ -8,6 +8,7 @@ var app = new Vue({
               label: 'Filter property 1',                            //This becomes visible to the user
               filterProperty: 'statuscodefilter',
               required: true,
+              selectionmode: 'single',
               values: [
                   {
                       label:'Filter value 1 (default)',
@@ -27,6 +28,7 @@ var app = new Vue({
           {
               label: 'Length',                            
               filterProperty: 'length',
+              selectionmode: 'single',
               values: [
                   {
                       label:'Length=18 (default)',
@@ -43,6 +45,7 @@ var app = new Vue({
           {
             label: 'Algorithm',                            
             filterProperty: 'algorithm',
+            selectionmode: 'single',
             values: [
                 {
                     label:'List',
